f5fb89f69772df4114b5df9a3b7d415906f3b7b2
Viya Atambayeva <102431891+viyamira@users.noreply.github.com>
2025-10-23 19:50:15 -0400

    Edit API

--- public/patients.js
+++ public/patients.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCKLMx3Vi1Lo_TB6D942OWdmtD_kihL9h4",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "https://nurse-websit-default-rtdb.firebaseio.com/",
-  projectId: "nurse-websit",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "1:982443926776:web:7bf7e73353311b6f5a870d"
+  apiKey: 
+  authDomain: 
+  databaseURL: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
 };
 
 firebase.initializeApp(firebaseConfig);
